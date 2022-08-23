57f6e58f4ab9757101ea48cfad80fa19afb9977f
Kirill Alyoshin <alyoshinML@gmail.com>
2022-08-23 15:37:51 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ClassCounter from "./components/ClassCounter";
+
+function App() {
+
+  return (
+    <div className="App">
+      <ClassCounter/>
+    </div>
+  );
+}
+
+export default App;
